a8a4435f76a3fb802a44310de5c741b89cac71c2
dvgodoy <dvgodoy@gmail.com>
2017-11-25 00:01:48 +0100

    charged/fire method

--- neurons.py
+++ neurons.py
@@ -4,50 +4,119 @@ from activations import Activation
 from losses import Loss
 import numpy as np
 import abc
+import uuid
 
-
-class Neuron(object):
+class Node(object):
     def __init__(self):
-        self.bias = np.zeros((1, 1))
-        self.db = np.zeros((1, 0))
-        self.weights = np.zeros((1, 0))
-        self.dw = np.zeros((1, 0))
-        self.activations = np.zeros((1, 0))
+        self.id = uuid.uuid4().hex
 
-        self.input_neurons = []
-        self.input_hashes = np.array([])
-        self.input_activated = np.array([])
+        self._input_nodes = []
+        self._input_ids = np.array([])
+        self._input_path_lengths = np.zeros(0)
 
-        self.output_neurons = []
-        self.output_hashes = np.array([])
-        self.output_feedback = np.array([])
+        self._output_nodes = []
+        self._output_ids = np.array([])
+        self._output_path_lengths = np.zeros(0)
 
-        self.gradients = np.zeros((1, 0))
-        self.inputs = np.zeros((1, 0))
+        self._layer = 0
 
-        self.m = 0
-        self.layer = 0
-        self.input_path_lengths = np.zeros(0)
-        self.output_path_lengths = np.zeros(0)
+    @property
+    def node_id(self):
+        return self.id
+
+    @property
+    def layer(self):
+        return self._layer
+
+    def _node_index(self, node_ids, node):
+        try:
+            idx = np.where(node_ids == node.node_id)[0][0]
+        except (IndexError, AttributeError):
+            idx = -1
+        return idx
+
+    def add_output(self, node):
+        self._output_nodes.append(node)
+        self._output_ids = np.append(self._output_ids, [[node.node_id]])
+        self._output_path_lengths = np.zeros(len(self._output_nodes))
+
+    def add_input(self, node):
+        self._input_nodes.append(node)
+        self._input_ids = np.append(self._input_ids, [[node.node_id]])
+        self._input_path_lengths = np.zeros(len(self._input_nodes))
+
+    def _initialize_forward(self, node=None, path_length=0):
+        if path_length > self.layer:
+            self._layer = path_length
+
+        i = self._node_index(self._input_ids, node)
+        if i >= 0:
+            self._input_path_lengths[i] = path_length
+
+        for node in self._output_nodes:
+            node._initialize_forward(self, path_length + 1)
+
+    def _initialize_backward(self, node=None, path_length=0):
+        i = self._node_index(self._output_ids, node)
+        if i >= 0:
+            self._output_path_lengths[i] = path_length
+
+        for node in self._input_nodes:
+            node._initialize_backward(self, path_length + 1)
+
+
+class Neuron(Node):
+    def __init__(self):
+        super(Neuron, self).__init__()
+        self._charged = False
+        self._updated = False
+        self._learning_rate = 0.3
+
+        self._bias = np.zeros((1, 1))
+        self._weights = np.zeros((1, 0))
+        self._activations = np.zeros((1, 0))
+
+        self._db = np.zeros((1, 0))
+        self._dw = np.zeros((1, 0))
+        self._dz = np.zeros((1, 0))
+        self._da = np.zeros((1, 0))
+
+        self._inputs = np.zeros((1, 0))
+        self._input_activated = np.array([])
+
+        self._gradients = np.zeros((1, 0))
+        self._output_feedback = np.array([])
+
+        self._auto_prop = False
+
+        self._m = 0
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @property
+    def bias(self):
+        return self._bias
+
+    @property
+    def activations(self):
+        return self._activations
 
     def update_parameters(self, learning_rate):
-        self.weights = self.weights - learning_rate * self.dw
-        self.bias = self.bias - learning_rate * self.db
+        self._weights = self._weights - learning_rate * self._dw
+        self._bias = self._bias - learning_rate * self._db
 
     def add_output(self, neuron):
-        self.output_neurons.append(neuron)
-        self.output_hashes = np.append(self.output_hashes, [[neuron.__hash__()]])
-        self.gradients = np.zeros((len(self.output_neurons), 0))
-        self.output_path_lengths = np.zeros(len(self.output_neurons))
-        self.output_feedback = np.zeros(len(self.output_neurons))
+        super(Neuron, self).add_output(neuron)
+        self._gradients = np.zeros((len(self._output_nodes), 0))
+        self._output_feedback = np.zeros(len(self._output_nodes))
 
     def add_input(self, neuron):
-        self.weights = np.append(self.weights, np.random.randn(1, 1) * 0.01).reshape(1, -1)
-        self.input_neurons.append(neuron)
-        self.input_hashes = np.append(self.input_hashes, [[neuron.__hash__()]])
-        self.inputs = np.zeros((len(self.input_neurons), 0))
-        self.input_path_lengths = np.zeros(len(self.input_neurons))
-        self.input_activated = np.zeros(len(self.input_neurons))
+        super(Neuron, self).add_input(neuron)
+        self._weights = np.append(self._weights, np.random.randn(1, 1) * 0.01).reshape(1, -1)
+        self._inputs = np.zeros((len(self._input_nodes), 0))
+        self._input_activated = np.zeros(len(self._input_nodes))
 
     def connect(self, neurons):
         assert neurons is not None
@@ -57,37 +126,11 @@ class Neuron(object):
 
         for neuron in neurons:
             # prevents connecting twice to same neuron
-            if self.__neuron_index(self.output_hashes, neuron) < 0:
+            if self._node_index(self._output_ids, neuron) < 0:
                 # adds destination neuron to outputlist
                 self.add_output(neuron)
                 # at the destination neuron, adds source neuron as input
                 neuron.add_input(self)
-
-    def _initialize_forward(self, input_neuron=None, path_length=0):
-        if path_length > self.layer:
-            self.layer = path_length
-
-        i = self.__neuron_index(self.input_hashes, input_neuron.__hash__())
-        if i >= 0:
-            self.input_path_lengths[i] = path_length
-
-        for neuron in self.output_neurons:
-            neuron._initialize_forward(self, path_length + 1)
-
-    def _initialize_backward(self, output_neuron=None, path_length=0):
-        i = self.__neuron_index(self.output_hashes, output_neuron.__hash__())
-        if i >= 0:
-            self.output_path_lengths[i] = path_length
-
-        for neuron in self.input_neurons:
-            neuron._initialize_backward(self, path_length + 1)
-
-    def __neuron_index(self, neurons_hashes, neuron):
-        try:
-            idx = np.where(neurons_hashes == neuron.__hash__())[0][0]
-        except IndexError:
-            idx = -1
-        return idx
 
     @abc.abstractmethod
     def forward_propagation(self):
@@ -98,55 +141,62 @@ class Neuron(object):
         pass
 
     def gradient_from(self, da, neuron, learning_rate):
-        i = self.__neuron_index(self.output_hashes, neuron)
+        i = self._node_index(self._output_ids, neuron)
         if i >= 0:
-            self.output_feedback[i] = 1.
-            if self.gradients.shape[1] != da.shape[1]:
-                self.gradients = np.zeros((len(self.output_neurons), da.shape[1]))
-            self.gradients[i, np.newaxis, :] = da
+            self._learning_rate = learning_rate
+            self._output_feedback[i] = 1.
+            if self._gradients.shape[1] != da.shape[1]:
+                self._gradients = np.zeros((len(self._output_nodes), da.shape[1]))
+            self._gradients[i, np.newaxis, :] = da
 
-            if np.all(self.output_feedback):
-                self.output_feedback -= 1.
-                self.back_propagation(learning_rate)
+            if np.all(self._output_feedback):
+                self._updated = True
+                if self._auto_prop:
+                    self.feedback()
+
+    def feedback(self):
+        if self._updated:
+            self._output_feedback -= 1.
+            self.back_propagation(self._learning_rate)
 
     def input_from(self, activations, neuron):
-        i = self.__neuron_index(self.input_hashes, neuron)
+        i = self._node_index(self._input_ids, neuron)
         if i >= 0:
-            self.input_activated[i] = 1.
-            if self.inputs.shape[1] != activations.shape[1]:
-                self.inputs = np.zeros((len(self.input_neurons), activations.shape[1]))
-            self.inputs[i, np.newaxis, :] = activations
+            self._input_activated[i] = 1.
+            if self._inputs.shape[1] != activations.shape[1]:
+                self._inputs = np.zeros((len(self._input_nodes), activations.shape[1]))
+            self._inputs[i, np.newaxis, :] = activations
 
-            if np.all(self.input_activated):
-                self.input_activated -= 1.
-                self.forward_propagation()
+            if np.all(self._input_activated):
+                self._charged = True
+                if self._auto_prop:
+                    self.fire()
+
+    def fire(self):
+        if self._charged:
+            self._input_activated -= 1.
+            self.forward_propagation()
 
 
 class Input(Neuron):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(Input, self).__init__()
-        self.activations = np.zeros((1, 0))
+        self._activations = np.zeros((1, 0))
 
     def __str__(self):
-        return 'Input Neuron {}'.format(self.activations.shape)
+        return 'Input Neuron {}'.format(self._activations.shape)
 
     def examples(self, X):
-        '''
-        Feeds m examples of a single feature to the neuron.
-
-        :param X: numpy array (1, m) containing m examples for a single feature
-        :return: None
-        '''
         assert isinstance(X, np.ndarray)
         assert X.shape[0] == 1
 
-        self.m = X.shape[1]
-        self.activations = X
+        self._m = X.shape[1]
+        self._activations = X
         self.forward_propagation()
 
     def forward_propagation(self):
-        for neuron in self.output_neurons:
-            neuron.input_from(self.activations, self)
+        for neuron in self._output_nodes:
+            neuron.input_from(self._activations, self)
 
     def back_propagation(self, learning_rate):
         pass
@@ -155,73 +205,82 @@ class Input(Neuron):
         self._initialize_forward()
 
 class Hidden(Neuron):
-    def __init__(self, activation_function):
+    def __init__(self, activation_function, **kwargs):
         super(Hidden, self).__init__()
         assert isinstance(activation_function, Activation)
 
-        self.activation_function = activation_function
-        self.inputs = np.zeros((1, 0))
+        self._activation_function = activation_function
+        self._inputs = np.zeros((1, 0))
 
     def __str__(self):
-        return 'Hidden Neuron {}'.format(self.inputs.shape)
+        return 'Hidden Neuron {}'.format(self._inputs.shape)
 
     def forward_propagation(self):
-        assert self.inputs.shape[1] > 0
-        assert self.weights.shape[1] == self.inputs.shape[0]
-        assert self.bias.shape == (1, 1)
+        assert self._inputs.shape[1] > 0
+        assert self._weights.shape[1] == self._inputs.shape[0]
+        assert self._bias.shape == (1, 1)
 
-        self.z = np.dot(self.weights, self.inputs) + self.bias
-        self.activations = self.activation_function.evaluate(self.z)
-        for neuron in self.output_neurons:
-            neuron.input_from(self.activations, self)
+        self._z = np.dot(self._weights, self._inputs) + self._bias
+        self._activations = self._activation_function.evaluate(self._z)
+        for neuron in self._output_nodes:
+            neuron.input_from(self._activations, self)
 
     def _compute_dz(self):
-        return np.sum(self.gradients, axis=0, keepdims=True) * self.activation_function.gradient(self.z)
+        return np.sum(self._gradients, axis=0, keepdims=True) * self._activation_function.gradient(self._z)
 
     def back_propagation(self, learning_rate):
-        self.dz = self._compute_dz()
-        self.dw = np.mean(self.dz * self.inputs, axis=1).reshape(1, -1)
-        self.db = np.mean(self.dz, keepdims=True)
-        da = np.dot(self.weights.T, self.dz)
+        self._dz = self._compute_dz()
+        self._dw = np.mean(self._dz * self._inputs, axis=1).reshape(1, -1)
+        self._db = np.mean(self._dz, keepdims=True)
+        self._da = np.dot(self._weights.T, self._dz)
         self.update_parameters(learning_rate)
-        for i, neuron in enumerate(self.input_neurons):
-            gradient = da[i, np.newaxis, :]
+        for i, neuron in enumerate(self._input_nodes):
+            gradient = self._da[i, np.newaxis, :]
             neuron.gradient_from(gradient, self, learning_rate)
 
 
 class Output(Hidden):
-    def __init__(self, activation_function, loss_function, learning_rate):
+    def __init__(self, activation_function, loss_function, learning_rate=0.3):
         super(Output, self).__init__(activation_function)
         assert isinstance(loss_function, Loss)
-        self.loss_function = loss_function
+        self._loss_function = loss_function
         if not callable(learning_rate):
             constant = learning_rate
             learning_rate = lambda epochs: constant
-        self.learning_rate = learning_rate
-        self.y = np.zeros((1, 0))
-        self.epochs = 0
-        self.cost = np.inf
+        self._learning_rate = learning_rate
+        self._y = np.zeros((1, 0))
+        self._epochs = 0
+        self._cost = np.inf
 
     def __str__(self):
-        return 'Output Neuron {}'.format(self.y.shape)
+        return 'Output Neuron {}'.format(self._y.shape)
+
+    @property
+    def cost(self):
+        return self._cost
 
     def responses(self, y):
         assert isinstance(y, np.ndarray)
         assert y.shape[0] == 1
-        self.y = y
+        self._y = y
 
     def forward_propagation(self):
         super(Output, self).forward_propagation()
-        self.cost = self.compute_cost()
-        self.epochs += 1
-        self.back_propagation(self.learning_rate(self.epochs))
+        self._cost = self.compute_cost()
+        self._epochs += 1
+        self._updated = True
+
+    def back_propagation(self, learning_rate=None):
+        learning_rate = self._learning_rate(self._epochs)
+        super(Output, self).back_propagation(learning_rate)
 
     def compute_cost(self):
-        assert self.activations.shape == self.y.shape
-        return np.mean(self.loss_function.compute(self.activations, self.y))
+        assert self._activations.shape == self._y.shape
+        self._output_feedback += 1.
+        return np.mean(self._loss_function.compute(self._activations, self._y))
 
     def _compute_dz(self):
-        return self.loss_function.gradient(self.activations, self.y)
+        return self._loss_function.gradient(self._activations, self._y)
 
     def initialize(self):
         self._initialize_backward()
--- tests/test_neurons.py
+++ tests/test_neurons.py
@@ -5,19 +5,79 @@ from losses import LogLoss
 from activations import SigmoidActivation, ReluActivation
 import numpy as np
 
+class Layer(object):
+    classes = ['Input', 'Hidden', 'Output']
+
+    def __init__(self, n_units, layer_type, **kwargs):
+        assert layer_type in self.classes
+        self.n_units = n_units
+        self.layer_type = layer_type
+        neuron = globals()[layer_type]
+        self.units = [neuron(**kwargs) for _ in range(n_units)]
+
+    def connect(self, layer):
+        assert isinstance(layer, Layer)
+        for source in self.units:
+            for destination in layer.units:
+                source.connect(destination)
+
+    def fire(self):
+        for unit in self.units:
+            unit.fire()
+
+    def feedback(self):
+        for unit in self.units:
+            unit.feedback()
+
+    def examples(self, X):
+        assert X.shape[0] == self.n_units
+        for i, unit in enumerate(self.units):
+            unit.examples(X[i, :])
+
+    def responses(self, Y):
+        assert Y.shape[0] == self.n_units
+        for i, unit in enumerate(self.units):
+            unit.responses(Y[i, :])
+
+    @property
+    def activations(self):
+        return np.array([unit.activations for unit in self.units])
+
+    @property
+    def weights(self):
+        return np.array([unit.weights for unit in self.units])
+
+    @property
+    def biases(self):
+        return np.array([unit.biases for unit in self.units])
+
+    @property
+    def cost(self):
+        return np.array([unit.cost for unit in self.units])
+
 if __name__ == '__main__':
     np.random.seed(13)
 
     sigmoid = SigmoidActivation()
     relu = ReluActivation()
+
+    l_i = Layer(2, 'Input')
+    l_h1 = Layer(2, 'Hidden', activation_function=relu)
+    l_h2 = Layer(2, 'Hidden', activation_function=relu)
+    l_o = Layer(1, 'Output', activation_function=sigmoid, loss_function=LogLoss(), learning_rate=0.3)
+    layers = [l_i, l_h1, l_h2, l_o]
+
+    l_i.connect(l_h1)
+    l_h1.connect(l_h2)
+    l_h2.connect(l_o)
+
     x1 = np.array([1,2,3], ndmin=2)
     x1 = x1 - np.mean(x1)
     x2 = np.array([2,0,7], ndmin=2)
-    #x2 = np.array([5,0,7], ndmin=2)
-    #x2 = np.array([7,0,7], ndmin=2)
     x2 = x2 - np.mean(x2)
     y = np.array([1,1,0], ndmin=2)
 
+    """
     i1 = Input()
     i2 = Input()
 
@@ -48,11 +108,30 @@ if __name__ == '__main__':
     i2.initialize()
     o.initialize()
 
+    neurons = sorted([h11, o, i1, i2, h12, h21, h22], key=lambda o: o.layer)
+    """
+
     epochs = 1000
     for _ in range(epochs):
-        o.responses(y)
-        i1.examples(x1)
-        i2.examples(x2)
+        l_i.examples(np.array([x1, x2]))
+        l_o.responses(np.array([y]))
+        for layer in layers:
+            layer.fire()
+        print(l_h1.weights)
+        #print(l_o.cost)
+        for layer in layers[::-1]:
+            layer.feedback()
+    print(l_o.activations)
+    print(l_o.activations.shape)
+    print(l_h1.weights.shape)
 
-        print(o.cost)
-    print(o.activations)
+    #for _ in range(epochs):
+        #o.responses(y)
+        #i1.examples(x1)
+        #i2.examples(x2)
+        #for neuron in neurons:
+        #    neuron.fire()
+        #print(o.cost)
+        #for neuron in neurons[::-1]:
+        #    neuron.feedback()
+    #print(o.activations)
